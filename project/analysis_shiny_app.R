b0752c597fef65dbd5f9ef880849b7634ec98115
reneends <117844129+reneends@users.noreply.github.com>
2022-12-13 18:06:03 +0100

    Added instructions for tab 3

--- project/analysis_shiny_app.R
+++ project/analysis_shiny_app.R
@@ -56,7 +56,14 @@ ui <- fluidPage(
       tab2(),
       tabPanel('3. Journey Distance',
                
-                 sidebarPanel(
+               wellPanel(
+                 h2('Instructions'),
+                 p("This tab allows the user to interactively analyze data about the average distance of bicikelj travels"),
+                shiny::tags$li('In the first step the user can filter the data based on good weather (no rain and more than 5.3C temparature)
+                               or bad weather (either rain, below 5.3C temparature, or both). '),
+                shiny::tags$li('In the second step the user can filter the data based on the day of the week.')),
+                 
+               sidebarPanel(
                    h3("Weather type"),
                    checkboxInput('showGoodweather', label = 'Good Weather', value = TRUE),
                    checkboxInput('showBadweather', label = 'Bad Weather', value = TRUE),
@@ -110,14 +117,7 @@ server <- function(input, output) {
 
 
   output$DistanceBarplot2 <- renderPlot({
-  #   showMonday <- slider_value
-  #   showTuesday <- input$dayoftheweek[2]
-  #   showWednesday <- input$dayoftheweek[3]
-  #   showThursday <- input$dayoftheweek[4]
-  #   showFriday <- input$dayoftheweek[5]
-  #   showSaturday <- input$dayoftheweek[6]
-  #   showSunday <- input$dayoftheweek[7]
-  #   
+ 
     disdata$weekday <- as.factor(disdata$weekday)
     slider_value <- input$dayoftheweek
 
